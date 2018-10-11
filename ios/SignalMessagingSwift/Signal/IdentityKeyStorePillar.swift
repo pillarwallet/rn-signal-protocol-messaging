6a0dcc5223cb47ce9f53df2652773dad17a5fabc
Deimantas Špučys <as.deimis@gmail.com>
2018-10-11 14:12:38 +0300

    trusted identity method force true explanation

--- ios/SignalMessagingSwift/Signal/IdentityKeyStorePillar.swift
+++ ios/SignalMessagingSwift/Signal/IdentityKeyStorePillar.swift
@@ -40,6 +40,14 @@ class IdentityKeyStorePillar: IdentityKeyStore {
         return true
     }
     
+    /*
+         This method returns force true as it didn't affect basic flow of Signal.
+     
+         On 2018-10-10 discovered (on iOS) that this method checks PreKey identity while
+         renewing PreKey over existing addresses in device (stale device flow).
+     
+         TODO: Get into more details at Signal structure level how this method works and prevent force true return.
+     */
     func isTrusted(identity: Data, for address: SignalAddress) -> Bool? {
         return true
 //        let identities = self.storage().get(for: .IDENTITES_JSON_FILENAME)
