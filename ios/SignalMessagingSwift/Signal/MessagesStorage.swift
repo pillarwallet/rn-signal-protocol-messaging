70cc4a7bdfac7effa89d94efd05c796a6c9bb30b
Deimantas Špučys <as.deimis@gmail.com>
2018-11-21 10:40:43 +0200

    added ios functionality for messages with tags

--- ios/SignalMessagingSwift/Signal/MessagesStorage.swift
+++ ios/SignalMessagingSwift/Signal/MessagesStorage.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class MessagesStorage: ProtocolStorage {
+    
+    
+  func getMessageStorageFilename(for tag: String) -> DownloadsType {
+    switch tag {
+        case "chat": return .MESSAGES_CHAT_JSON_FILENAME
+        case "tx-note": return .MESSAGES_TXNOTE_JSON_FILENAME
+        default: return .MESSAGES_OTHER_JSON_FILENAME
+    }
+  }
   
-  func save(message: ParsedMessageDTO, for username: String) {
-    var localJson = self.getAllMessages()
+  func save(message: ParsedMessageDTO, for username: String, tag: String) {
+    var localJson = self.getAllMessages(for: tag)
     var userData = [String : Any]()
     if let userDataFile = localJson[username] as? [String : Any] {
       userData = userDataFile
@@ -25,11 +34,11 @@ class MessagesStorage: ProtocolStorage {
     parsedMessages.append(message.dictionary)
     userData["messages"] = parsedMessages
     localJson[username] = userData
-    self.save(array: ["allMessages" : localJson], type: .MESSAGES_JSON_FILENAME)
+    self.save(array: ["allMessages" : localJson], type: getMessageStorageFilename(for: tag))
   }
   
-  func getMessages(for username: String) -> [ParsedMessageDTO] {
-    let localJson = self.getAllMessages()
+  func getMessages(for username: String, tag: String) -> [ParsedMessageDTO] {
+        let localJson = self.getAllMessages(for: tag)
     guard let userDict = localJson[username] as? [String : Any], let userMessagesDict = userDict["messages"] as? [[String : Any]] else {
       return [ParsedMessageDTO]()
     }
@@ -41,8 +50,8 @@ class MessagesStorage: ProtocolStorage {
     return userMessages
   }
   
-  func getUnreadCount(for username: String) -> Int {
-    let localJson = self.getAllMessages()
+  func getUnreadCount(for username: String, tag: String) -> Int {
+    let localJson = self.getAllMessages(for: tag)
     guard let userDict = localJson[username] as? [String : Any], let unreadCount = userDict["unreadCount"] as? Int else {
       return 0
     }
@@ -50,9 +59,9 @@ class MessagesStorage: ProtocolStorage {
     return unreadCount
   }
   
-  func saveUnreadCount(for username: String, count : Int) {
-    let currentCount = self.getUnreadCount(for: username)
-    var localJson = self.getAllMessages()
+  func saveUnreadCount(for username: String, count: Int, tag: String) {
+    let currentCount = self.getUnreadCount(for: username, tag: tag)
+    var localJson = self.getAllMessages(for: tag)
     var userData = [String : Any]()
     if let userDataFile = localJson[username] as? [String : Any] {
       userData = userDataFile
@@ -60,11 +69,11 @@ class MessagesStorage: ProtocolStorage {
     
     userData["unreadCount"] = count + currentCount
     localJson[username] = userData
-    self.save(array: ["allMessages" : localJson], type: .MESSAGES_JSON_FILENAME)
+    self.save(array: ["allMessages" : localJson], type: getMessageStorageFilename(for: tag))
   }
   
-  func getAllMessages() -> [String : Any] {
-    let localJson = self.get(for: .MESSAGES_JSON_FILENAME)
+  func getAllMessages(for tag: String) -> [String : Any] {
+    let localJson = self.get(for: getMessageStorageFilename(for: tag))
     guard let list = localJson["allMessages"] as? [String : Any] else {
       return [String : Any]()
     }
@@ -72,10 +81,10 @@ class MessagesStorage: ProtocolStorage {
     return list
   }
   
-  func deleteContactMessages(for username: String) {
-    var localJson = self.getAllMessages()
+  func deleteContactMessages(for username: String, tag: String) {
+    var localJson = self.getAllMessages(for: tag)
     localJson[username] = nil
-    self.save(array: ["allMessages" : localJson], type: .MESSAGES_JSON_FILENAME)
+    self.save(array: ["allMessages" : localJson], type: getMessageStorageFilename(for: tag))
   }
 
 }
--- ios/SignalMessagingSwift/Signal/RNSignalClientModule.swift
+++ ios/SignalMessagingSwift/Signal/RNSignalClientModule.swift
@@ -93,25 +93,25 @@ class RNSignalClientModule: NSObject {
         resolve("ok")
     }
     
-    @objc func deleteContactMessages(_ username: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        MessagesStorage().deleteContactMessages(for: username)
-        self.signalClient.deleteContactPendingMessages(username: username, success: { (success) in
+    @objc func deleteContactMessages(_ username: String, messageTag: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        MessagesStorage().deleteContactMessages(for: username, tag: messageTag)
+        self.signalClient.deleteContactPendingMessages(username: username, messageTag: messageTag, success: { (success) in
             resolve(success)
         }) { (error, message) in
             reject(error, message, nil)
         }
     }
     
-    @objc func receiveNewMessagesByContact(_ username: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        self.signalClient.getContactMessages(username: username, decodeAndSave: true, success: { (success) in
+    @objc func receiveNewMessagesByContact(_ username: String, messageTag: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        self.signalClient.getContactMessages(username: username, messageTag: messageTag, decodeAndSave: true, success: { (success) in
             resolve(success)
         }) { (error, message) in
             reject(error, message, nil)
         }
     }
     
-    @objc func getChatByContact(_ username: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        let sortedMessages = MessagesStorage().getMessages(for: username).sorted { (messageOne, messageTwo) -> Bool in
+    @objc func getMessagesByContact(_ username: String, messageTag: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        let sortedMessages = MessagesStorage().getMessages(for: username, tag: messageTag).sorted { (messageOne, messageTwo) -> Bool in
             return messageOne.savedTimestamp > messageTwo.savedTimestamp
         }.compactMap { (message) -> [String : Any]? in
             return message.dictionary
@@ -119,21 +119,19 @@ class RNSignalClientModule: NSObject {
         
         if let data = try? JSONSerialization.data(withJSONObject: sortedMessages, options: .prettyPrinted) {
             let jsonSring = String(data: data, encoding: .utf8)
-            print(jsonSring)
             resolve(jsonSring)
         }
     }
     
-    @objc func getExistingChats(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        if let data = try? JSONSerialization.data(withJSONObject: self.signalClient.getAllContactMessages(), options: .prettyPrinted) {
+    @objc func getExistingMessages(_ messageTag: String, resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        if let data = try? JSONSerialization.data(withJSONObject: self.signalClient.getAllContactMessages(messageTag: messageTag), options: .prettyPrinted) {
             let jsonSring = String(data: data, encoding: .utf8)
-            print(jsonSring)
             resolve(jsonSring)
         }
     }
     
-    @objc func getUnreadMessagesCount(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        self.signalClient.getContactMessages(username: "", decodeAndSave: false, success: { (dict) in
+    @objc func getUnreadMessagesCount(_ messageTag: String, resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        self.signalClient.getContactMessages(username: "", messageTag: messageTag, decodeAndSave: false, success: { (dict) in
             if let data = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted) {
                 let jsonSring = String(data: data, encoding: .utf8)
                 resolve(jsonSring)
@@ -145,8 +143,8 @@ class RNSignalClientModule: NSObject {
         }
     }
     
-    @objc func sendMessageByContact(_ username: String, messageString: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        self.signalClient.sendMessage(username: username, messageString: messageString, success: { (success) in
+    @objc func sendMessageByContact(_ username: String, messageString: String, messageTag: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        self.signalClient.sendMessage(username: username, messageString: messageString, messageTag: messageTag, success: { (success) in
             resolve(success)
         }) { (error, message) in
             reject(error, message, nil)
