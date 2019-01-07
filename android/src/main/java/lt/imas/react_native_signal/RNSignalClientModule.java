dcb9b898ef37d7fbe694fc2b72496eb9dbf01e24
Deimantas Špučys <as.deimis@gmail.com>
2019-01-07 22:19:02 +0200

    android delete method param fix

--- android/src/main/java/lt/imas/react_native_signal/RNSignalClientModule.java
+++ android/src/main/java/lt/imas/react_native_signal/RNSignalClientModule.java
@@ -269,7 +269,7 @@ public class RNSignalClientModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void deleteSignalMessage(String username, long timestamp, final Promise promise) {
+    public void deleteSignalMessage(String username, int timestamp, final Promise promise) {
         signalClient.deleteSignalMessage(username, timestamp);
         promise.resolve("ok");
     }
